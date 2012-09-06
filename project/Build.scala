15944e2e9a90f58640f6128e3a871e5486d192fe
Ken Lim <kenneth.lim@guardian.co.uk>
2012-09-06 17:26:33 +0100

    Files are actually hashed now, and we have an index file.

--- project/Build.scala
+++ project/Build.scala
@@ -7,6 +7,7 @@ object ExperimentalBuild extends Build {
   lazy val project = Project("project", file("."),
     settings = buildSettings
   ).settings(
+    unmanagedResourceDirectories in Compile <+= baseDirectory / "booyah",
     resourceGenerators in Compile <+= generateHashedResourcesWithIndexFile)
 
   lazy val buildSettings: Seq[Setting[_]] = Defaults.defaultSettings ++ Seq(
@@ -21,7 +22,9 @@ object ExperimentalBuild extends Build {
     (unmanagedResourceDirectories in Compile, resourceManaged in Compile, cacheDirectory, streams) map {
       (resourceDirs, target, cache, s) =>
         val cacheFolder = cache / "hashed-resources"
+        val indexFile = target / "hashindex.properties"
 
+        s.log.info("This is the unmanaged folders:" + resourceDirs)
         // Forcing it to go to "
         val mapOfOriginalToTarget = resourceDirs map {dir =>
             ((dir ** "*") filter { _.isFile}).get x rebase(dir, target)
@@ -31,8 +34,15 @@ object ExperimentalBuild extends Build {
 
         val mapOfOriginalToHashedFiles = mapOfOriginalToTarget map { case(original, dest) =>
             val nameComponents = dest.getPath.split('.')
-            val newName = nameComponents.dropRight(1).toList ::: "hash" :: nameComponents.last :: Nil
+            val newName = nameComponents.dropRight(1).toList ::: hash(original) :: nameComponents.last :: Nil
             (original, file(newName.mkString(".")))
+        }
+
+
+        mapOfOriginalToHashedFiles foreach { case(original, dest) =>
+          val resourceFile = ((original x relativeTo(resourceDirs)) head)._2
+          val hashedFile = dest.relativeTo(target).get
+          IO.write(indexFile, "%s=%s\n".format(resourceFile, hashedFile, IO.defaultCharset, true))
         }
 
         s.log.info("map of original to hashed files\n" + mapOfOriginalToHashedFiles)
@@ -42,8 +52,21 @@ object ExperimentalBuild extends Build {
         val result = mapOfOriginalToHashedFiles map {case(original, dest) => dest}
 
         s.log.info("Final Output\n" + result)
-        result
+        Seq(indexFile) ++ result
     }
+
+
+  private val digester = java.security.MessageDigest.getInstance("SHA-1")
+
+  private def hash(file: File): String = {
+    val source = scala.io.Source.fromFile(file, "latin1")
+    val bytes = source.map(_.toByte).toArray
+    source.close()
+    val byteData = digester.digest(bytes)
+    byteData.foldLeft("") {
+      (a, b) => a + (Integer.toString((b & 0xff) + 0x100, 16).substring(1))
+    }.take(8)
+  }
 }
 
 
